8307302d2488c64d2bf61b795e3da2fcb0b7775e
thiyangi28 <thiyangip24@gmail.com>
2026-05-15 14:56:34 +0530

    feat: define resource routes for buses using BusController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BusController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('buses', BusController::class);
