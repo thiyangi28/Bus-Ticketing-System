310268a4efe39f2d3a7d8fdd3b2bd65ee6d33e4a
thiyangi28 <thiyangip24@gmail.com>
2026-05-15 12:27:12 +0530

    feat: create routes table for bus routes management

---
+++ database/migrations/2026_05_15_064803_create_routes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('route_number')->unique();
+            $table->string('start_point');
+            $table->string('end_point');
+            $table->integer('distance') ->nullable(); 
+            $table->text('duration')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes');
+    }
+};
