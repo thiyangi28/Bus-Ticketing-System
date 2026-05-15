5019be4bd70078fbd441d4fe694e6225175f6189
thiyangi28 <thiyangip24@gmail.com>
2026-05-15 12:14:06 +0530

    feat: create buses table with necessary fields

--- database/migrations/2026_05_14_152535_create_buses_table.php
+++ database/migrations/2026_05_14_152535_create_buses_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('buses', function (Blueprint $table) {
             $table->id();
+            $table->string('bus_number')->unique();
+            $table->string('model')->nullable();
+            $table->integer('capacity'); // Number of seats
+            $table->enum('type', ['AC', 'Non-AC', 'Luxury'])->default('Non-AC');
+            $table->boolean('status')->default(true); // Active or Maintenance
             $table->timestamps();
         });
     }
